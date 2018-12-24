33504b21c36c999dab3e3195e579dc054dfb16f3
Forsina <stojanovska.frosina@students.finki.ukim.mk>
2018-12-24 19:41:22 +0100

    Created recources table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                   Welcome Bitches :)
                 </div>
 
                 <div class="links">
